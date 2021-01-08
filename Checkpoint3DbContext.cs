d74d8c6fa34c30c08994c9f90f9406c8483be404
Ly françois <67745643+LyFrancois@users.noreply.github.com>
2021-01-08 12:51:34 +0100

    Add files via upload

---
+++ Checkpoint3DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkpoint_3
+{
+    public class Checkpoint3DbContext : DbContext
+    {
+        public Checkpoint3DbContext(DbContextOptions<Checkpoint3DbContext> options)
+          : base(options)
+        {
+        }
+
+        public Checkpoint3DbContext()
+        {
+        }
+    }
+}
